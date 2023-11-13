165feea38c3ef39c459b1d0374fe6815c95ee6fc
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 15:42:38 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,22 +2,24 @@
 	import '../app.css';
 </script>
 
-<div class="min-h-screen">
-	<slot />
+<div class="bg-cards bg-fixed bg-cover">
+	<div class="min-h-screen bg-base-100 bg-opacity-95">
+		<slot />
 
-	<footer
-		class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh]"
-	>
-		<nav class="grid grid-flow-col gap-4">
-			<a href="/about" class="link link-hover">About us</a>
-			<a href="/terms" class="link link-hover">Terms of use</a>
-			<a href="/privacy" class="link link-hover">Privacy policy</a>
-			<a href="/disclaimer" class="link link-hover">Disclaimer</a>
-			<a href="/credits" class="link link-hover">Credits</a>
-			<a href="/contact" class="link link-hover">Contact</a>
-		</nav>
-		<aside>
-			<p>Copyright © 2023 - All right reserved by playcards.games</p>
-		</aside>
-	</footer>
+		<footer
+			class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh]"
+		>
+			<nav class="grid grid-flow-col gap-4">
+				<a href="/about" class="link link-hover">About us</a>
+				<a href="/terms" class="link link-hover">Terms of use</a>
+				<a href="/privacy" class="link link-hover">Privacy policy</a>
+				<a href="/disclaimer" class="link link-hover">Disclaimer</a>
+				<a href="/credits" class="link link-hover">Credits</a>
+				<a href="/contact" class="link link-hover">Contact</a>
+			</nav>
+			<aside>
+				<p>Copyright © 2023 - All right reserved by playcards.games</p>
+			</aside>
+		</footer>
+	</div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			backgroundImage: {
+				'cards': "url('/src/lib/assets/cards.jpg')"
+			}
+		}
 	},
 	plugins: [require('daisyui')]
 };
