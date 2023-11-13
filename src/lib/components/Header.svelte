d86ae970ff5749d6ca5e89272567802963b24c4d
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 13:14:23 +0100

    layout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,9 +4,33 @@
 
 <div class="navbar bg-base-200 px-4">
 	<div class="navbar-start">
+		<div class="dropdown">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label tabindex="0" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/></svg
+				>
+			</label>
+			<slot name="navbar-center-mobile" />
+		</div>
 		<a href="/">
+			<!-- svelte-ignore a11y-missing-attribute -->
 			<img src={logo} />
 		</a>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<slot name="navbar-center-desktop" />
 	</div>
 	<div class="navbar-end space-x-4">
 		<label class="swap">
--- src/routes/(games)/rageinside/+layout.svelte
+++ src/routes/(games)/rageinside/+layout.svelte
@@ -2,6 +2,11 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
-<Header />
+<Header>
+	<ul slot="navbar-center-desktop" class="menu menu-horizontal px-1 py-0">
+		<li><a href="play">Play</a></li>
+		<li><a href="ruleset">Rules</a></li>
+	</ul>
+</Header>
 
 <slot />
