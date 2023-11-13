3bc559be5d22e41258cdee88752a45de0b339f5c
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 11:49:59 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 		class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh]"
 	>
 		<nav class="grid grid-flow-col gap-4">
-			<a href="/" class="link link-hover">About us</a>
+			<a href="/about" class="link link-hover">About us</a>
 			<a href="/" class="link link-hover">Terms of use</a>
 			<a href="/" class="link link-hover">Privacy policy</a>
 			<a href="/" class="link link-hover">Disclaimer</a>
