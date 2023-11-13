d0bdd851df8907a770e706a0b935012f47dac9ec
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 11:15:27 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,12 +12,12 @@
 		class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh]"
 	>
 		<nav class="grid grid-flow-col gap-4">
-			<a class="link link-hover">About us</a>
-			<a class="link link-hover">Terms of use</a>
-			<a class="link link-hover">Privacy policy</a>
-			<a class="link link-hover">Disclaimer</a>
-			<a class="link link-hover">Credits</a>
-			<a class="link link-hover">Contact</a>
+			<a href="/" class="link link-hover">About us</a>
+			<a href="/" class="link link-hover">Terms of use</a>
+			<a href="/" class="link link-hover">Privacy policy</a>
+			<a href="/" class="link link-hover">Disclaimer</a>
+			<a href="/" class="link link-hover">Credits</a>
+			<a href="/" class="link link-hover">Contact</a>
 		</nav>
 		<aside>
 			<p>Copyright © 2023 - All right reserved by playcards.games</p>
