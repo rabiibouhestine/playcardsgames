63488ce94a798e285e8c8470642b08732096c029
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 10:12:34 +0100

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">Play Cards Games</h1>
+<p>Enjoy amazing solitaire cards games right from your browser</p>
+<button class="btn">Button</button>
