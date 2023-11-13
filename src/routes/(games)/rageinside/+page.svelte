ab2665037c33f2e859313984720bba4e9caaa927
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 15:57:18 +0100

    layout

--- src/routes/(games)/rageinside/+page.svelte
+++ src/routes/(games)/rageinside/+page.svelte
@@ -1,9 +1,16 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
+	import ArrowDownIcon from '$lib/components/ArrowDownIcon.svelte';
 </script>
 
-<Header title="Rage Inside" />
-
-<div class="grid justify-items-center space-y-6 mt-10">
-	<h1 class="text-5xl font-bold">Rage Inside Game</h1>
+<div class="min-h-screen bg-red-200">
+	<Header title="Rage Inside" />
+	<div id="pixi" />
+	<div class="w-full grid justify-items-center absolute bottom-2">
+		<h1 class="text-xl font-bold">ABOUT THE GAME</h1>
+		<ArrowDownIcon />
+	</div>
 </div>
+<article>
+	<h1 class="text-5xl font-bold">Rage Inside Game</h1>
+</article>
