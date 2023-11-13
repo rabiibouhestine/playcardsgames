464f6273c39a29328d95e899b1fb4126a610932f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-13 10:28:24 +0100

    added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,21 @@
     import "../app.css";
   </script>
   
-  <slot />
+  <div class="min-h-screen">
+    
+      <slot />
+    
+      <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh]">
+        <nav class="grid grid-flow-col gap-4">
+          <a class="link link-hover">About us</a>
+          <a class="link link-hover">Terms of use</a>
+          <a class="link link-hover">Privacy policy</a>
+          <a class="link link-hover">Disclaimer</a>
+          <a class="link link-hover">Contact</a>
+        </nav> 
+        <aside>
+          <p>Copyright © 2023 - All right reserved by playcards.games</p>
+        </aside>
+      </footer>
+      
+  </div>
