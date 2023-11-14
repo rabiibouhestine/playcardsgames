bf52faca03c1b9c42b071c1024b523bff652cc19
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-14 20:42:39 +0100

    layout

---
+++ src/lib/games/utils/Engine.js
@@ -0,0 +1,37 @@
+export class Card {
+
+}
+
+export class Pile {
+    constructor() {
+
+    }
+
+    removeCards() {
+        const removedCards = [];
+        return removedCards;
+    }
+
+    addCards(cards) {
+
+    }
+}
+
+export class Stack {
+    constructor() {
+
+    }
+
+    adjustCards() {
+
+    }
+
+    removeCards() {
+        const removedCards = [];
+        return removedCards;
+    }
+
+    addCards(cards) {
+
+    }
+}
