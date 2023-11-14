2382c09b755e21da0606ae82d8d40088988b1e13
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-14 15:32:33 +0100

    layout

--- src/lib/games/kingsmustdie/Game.js
+++ src/lib/games/kingsmustdie/Game.js
@@ -1,41 +1,11 @@
 import * as PIXI from "pixi.js";
+import { App } from "../utils/App";
 import { Layout } from "./Layout";
 
-export class Game {
+export class Game extends App {
     constructor(canvasRef) {
-        this.canvasRef = canvasRef;
-
-        this.app = new PIXI.Application({
-            // resizeTo: window,
-            width: 720,
-            height: 720,
-            resolution: window.devicePixelRatio || 1,
-            autoDensity: true,
-            backgroundColor: 0x475569
-        });
+        super(canvasRef, 0x475569);
 
         this.layout = new Layout(this.app);
-
-        this.resize();
-        window.addEventListener('resize', () => {this.resize()});
-        this.canvasRef.appendChild(this.app.view);
     }
-
-    resize() {
-        console.log("this.canvasRef.offsetHeight:", this.canvasRef.offsetHeight);
-        const canvasWidth = this.canvasRef.offsetWidth;
-        const canvasHeight = this.canvasRef.offsetHeight;
-        const stageScale = Math.min(canvasWidth, canvasHeight) / 720;
-
-        // Update renderer dimensions
-        this.app.renderer.resize(canvasWidth, canvasHeight);
-
-        // Update stage scale
-        this.app.stage.scale.set(stageScale);
-
-        // Center the stage
-        this.app.stage.x = (canvasWidth - 720 * stageScale ) / 2;
-        // this.app.stage.y = (canvasHeight - 720 * stageScale ) / 2;
-    }
-
 }
---
+++ src/lib/games/utils/App.js
@@ -0,0 +1,37 @@
+import * as PIXI from "pixi.js";
+
+export class App {
+    constructor(canvasRef, backgroundColor) {
+        this.canvasRef = canvasRef;
+
+        this.app = new PIXI.Application({
+            // resizeTo: window,
+            width: 720,
+            height: 720,
+            resolution: window.devicePixelRatio || 1,
+            autoDensity: true,
+            backgroundColor: backgroundColor
+        });
+
+        this.resize();
+        window.addEventListener('resize', () => {this.resize()});
+        this.canvasRef.appendChild(this.app.view);
+    }
+
+    resize() {
+        const canvasWidth = this.canvasRef.offsetWidth;
+        const canvasHeight = this.canvasRef.offsetHeight;
+        const stageScale = Math.min(canvasWidth, canvasHeight) / 720;
+
+        // Update renderer dimensions
+        this.app.renderer.resize(canvasWidth, canvasHeight);
+
+        // Update stage scale
+        this.app.stage.scale.set(stageScale);
+
+        // Center the stage
+        this.app.stage.x = (canvasWidth - 720 * stageScale ) / 2;
+        // this.app.stage.y = (canvasHeight - 720 * stageScale ) / 2;
+    }
+
+}
