ccc8379d4270f67a1de82c8341838a1dd382c209
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-14 10:14:18 +0100

    layout

--- src/routes/(games)/kingsmustdie/+page.svelte
+++ src/routes/(games)/kingsmustdie/+page.svelte
@@ -7,7 +7,7 @@
 	<Header title="Kings Must Die" />
 	<div id="pixi" />
 	<div class="w-full grid justify-items-center absolute bottom-0">
-		<h1 class="text-xl font-bold">ABOUT THE GAME</h1>
+		<h1 class="text-md font-bold">ABOUT THE GAME</h1>
 		<ArrowDownIcon />
 	</div>
 </div>
