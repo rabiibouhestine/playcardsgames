e30da9042dd81ce90048fb7c604517a58f2ef650
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-17 10:52:40 +0100

    engine

--- src/lib/games/utils/Cards.js
+++ src/lib/games/utils/Cards.js
@@ -73,6 +73,9 @@ export class Cards {
     }
 
     shuffleCards() {
-
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
     }
 }
