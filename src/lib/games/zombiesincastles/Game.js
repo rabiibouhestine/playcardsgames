b29198b1dba7e80895c59fa1d63b72c893f0db53
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-17 13:51:25 +0100

    engine

--- src/lib/games/zombiesincastles/Game.js
+++ src/lib/games/zombiesincastles/Game.js
@@ -30,7 +30,7 @@ export class Game extends App {
 
         // GAME SETUP
 
-        const drawDeck = dealer.shuffleCards([
+        const pileDeck = dealer.shuffleCards([
             'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS',
             'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC',
             'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH',
@@ -72,7 +72,7 @@ export class Game extends App {
 
         this.drawPile = new Cards(this.app, this.sheet, {
             type: 'pile',
-            faceNames: drawDeck,
+            faceNames: pileDeck,
             position: {x: 666, y: 277},
             onPointerdown: this.handleCardClick
         });
@@ -82,7 +82,8 @@ export class Game extends App {
             type: 'tableau',
             gap: 8,
             position: {x: 360, y: 467},
-            isInteractive: true
+            isInteractive: true,
+            onPointerdown: this.handleCardClick
         });
 
         const startingHandCards = this.drawPile.removeCards(8);
