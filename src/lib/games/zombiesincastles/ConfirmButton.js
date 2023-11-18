0a3cacfdd44264cf30863c2efb2c06a40a199c2f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-18 20:55:11 +0100

    game

--- src/lib/games/zombiesincastles/ConfirmButton.js
+++ src/lib/games/zombiesincastles/ConfirmButton.js
@@ -8,15 +8,31 @@ export class ConfirmButton {
 
         this.app = app;
 
+        // Define button graphic
         this.graphic = new PIXI.Graphics();
         this.graphic.beginFill(0x000000, 0.25);
-        this.graphic.drawRoundedRect(260, 568, 200, 50, 8);
+        this.graphic.drawRoundedRect(-100, -25, 200, 50, 8);
         this.graphic.endFill();
         
-        this.graphic.eventMode = 'static';
-        this.graphic.cursor = 'pointer';
-        this.graphic.on('pointerdown', onPointerDown);
+        // Define button label
+        this.label = new PIXI.Text("test", {
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            fontSize: 16,
+            fill: 0xFFFFFF,
+            align: 'center'
+        });
+        this.label.anchor.set(0.5);
 
-        this.app.stage.addChild(this.graphic);
+        // Define Button Container
+        this.container = new PIXI.Container();
+        this.container.eventMode = 'static';
+        this.container.cursor = 'pointer';
+        this.container.on('pointerdown', onPointerDown);
+        this.container.x = 360;
+        this.container.y = 585;
+        this.container.addChild(this.graphic);
+        this.container.addChild(this.label);
+        this.app.stage.addChild(this.container);
     }
 }
