e7ef4bda256e3822c3547f61c00e386470102598
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-21 21:52:40 +0100

    confirm button updates

--- src/lib/games/zombiesincastles/ConfirmButton.js
+++ src/lib/games/zombiesincastles/ConfirmButton.js
@@ -6,7 +6,8 @@ export class ConfirmButton {
 
         // Define button graphic
         this.graphic = new PIXI.Graphics();
-        this.graphic.beginFill(0x000000, 0.25);
+        this.graphic.lineStyle(2, 0x000000, 0.25);
+        this.graphic.beginFill(0xFFFFFF);
         this.graphic.drawRoundedRect(-125, -25, 250, 50, 8);
         this.graphic.endFill();
         
@@ -30,6 +31,10 @@ export class ConfirmButton {
         this.container.addChild(this.graphic);
         this.container.addChild(this.label);
         this.app.stage.addChild(this.container);
+
+        this.container
+            .on('pointerover', () => {this.graphic.alpha = 0.75})
+            .on('pointerout', () => {this.graphic.alpha = 1})
     }
 
     update(phase, selectionValue = 0) {
@@ -38,16 +43,22 @@ export class ConfirmButton {
                 this.label.text = "Deal " + selectionValue + " damage";
                 this.container.eventMode = 'static';
                 this.container.cursor = 'pointer';
+                this.graphic.tint = 0x42a5f5;
+                this.graphic.alpha = 1;
                 break;
             case 'discard':
                 this.label.text = "Take " + selectionValue + " damage";
                 this.container.eventMode = 'static';
                 this.container.cursor = 'pointer';
+                this.graphic.tint = 0x8d6e63;
+                this.graphic.alpha = 1;
                 break;
             case 'resolving':
                 this.label.text = "Resolving effects";
                 this.container.eventMode = 'none';
                 this.container.cursor = 'default';
+                this.graphic.tint = 0x000000;
+                this.graphic.alpha = 0.25;
                 break;
             default:
                 break;
