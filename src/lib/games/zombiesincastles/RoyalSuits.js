a8c89a34ff675a5281d12cedd733fc974ceeec2b
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-22 17:47:55 +0100

    royal suits

---
+++ src/lib/games/zombiesincastles/RoyalSuits.js
@@ -0,0 +1,15 @@
+import * as PIXI from "pixi.js";
+
+export class RoyalSuits {
+    constructor(modalContainer) {
+        this.modalContainer = modalContainer;
+    }
+
+    setSuit(suit, isOn) {
+
+    }
+
+    setAllSuits(isOn) {
+
+    }
+}
