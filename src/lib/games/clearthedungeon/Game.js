f01fa80bb99aa43fc2609fb956aa2ad69f7e5a7f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-26 01:08:07 +0100

    removed jokers

--- src/lib/games/clearthedungeon/Game.js
+++ src/lib/games/clearthedungeon/Game.js
@@ -56,8 +56,7 @@ export class Game extends App {
             'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS',
             'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC',
             'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH',
-            'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD',
-            'J1', 'J2'
+            'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD'
         ]);
 
         // make monster deck
