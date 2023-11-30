2ef825478d69f25d4a5d9aae6a647a45e1a0c03b
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-30 14:18:47 +0100

    scoundrel

--- src/lib/games/scoundrel/Game.js
+++ src/lib/games/scoundrel/Game.js
@@ -34,6 +34,68 @@ export class Game extends App {
 
         // add mattress
         this.mattress = new Mattress(this.mattressContainer);
+
+        // make dungeon pile deck
+        const dungeonPileDeck = this.dealer.shuffleCards([
+            'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'KD', 'KS', 'KH', 'KC',
+            'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'QD', 'QS', 'QH', 'QC',
+            'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JD', 'JS', 'JH', 'JC',
+            'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD'
+        ]);
+
+        // add dungeon pile
+        this.dungeonPile = new Cards(this.gameContainer, this.spritesheet, paramsAtlas, {
+            type: 'pile',
+            faceNames: dungeonPileDeck,
+            position: {x: 60, y: 171},
+            faceUp: false,
+            counter: true
+        });
+
+        // add discard pile
+        this.discardPile = new Cards(this.gameContainer, this.spritesheet, paramsAtlas, {
+            type: 'pile',
+            faceNames: [],
+            position: {x: 660, y: 171},
+            faceUp: false,
+            counter: true
+        });
+
+        // add room tableau
+        this.roomTableau = new Cards(this.gameContainer, this.spritesheet, paramsAtlas, {
+            name: 'room',
+            type: 'tableau',
+            gap: 20,
+            position: {x: 360, y: 171},
+            faceUp: true,
+            isInteractive: true,
+            // onPointerUp: this.handleCardClick.bind(this),
+            // onPointerOver: this.handleCardOver.bind(this),
+            // onPointerOut: this.handleCardOut.bind(this)
+        });
+
+        // add weapon Stack
+        this.weaponStack = new Cards(this.gameContainer, this.spritesheet, paramsAtlas, {
+            name: 'weapon',
+            type: 'stackH',
+            gap: 40,
+            position: {x: 360, y: 442},
+            faceUp: true,
+            // onPointerUp: this.handleCardClick.bind(this),
+            // onPointerOver: this.handleCardOver.bind(this),
+            // onPointerOut: this.handleCardOut.bind(this)
+        });
+
+        // draw first room
+        this.dealer.moveCards({
+            nbCards: 4,
+            source: this.dungeonPile ,
+            destination: this.roomTableau,
+            positionSource: 'top',
+            positionDestination: 'bottom',
+            inSequence: true,
+            immediate: false
+        });
     }
 
 }
