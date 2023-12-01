186d205e504c1f8d66c03cffc7f0818b4af4c7a9
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-01 15:37:09 +0100

    scoundrel

--- src/lib/games/utils/Cards.js
+++ src/lib/games/utils/Cards.js
@@ -52,7 +52,7 @@ export class Cards {
         this.adjustCards({immediate: true, faceUp: faceUp});
 
         this.counter = new Number(gameContainer, position, this.cards.length, { visible: counter, fontSize: 24 });
-        this.counter.valueText.zIndex = 60;
+        this.counter.valueText.zIndex = this.zIndex + 60;
     }
 
     adjustCards({immediate = false}) {
