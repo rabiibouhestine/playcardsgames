fab593eed1b112a8dba43561d151aa1cfca3e041
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-03 13:53:18 +0100

    style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,19 +14,17 @@
 	});
 </script>
 
-<div class="bg-cards bg-fixed bg-cover">
-	<div class="min-h-screen bg-base-100/90 backdrop-blur-sm">
-		<slot />
+<div class="min-h-screen bg-base-100">
+	<slot />
 
-		<footer
-			class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh] gap-y-8"
-		>
-			<nav class="flex flex-wrap justify-center gap-4">
-				<a href="/terms" class="link link-hover">Terms of use</a>
-				<a href="/privacy" class="link link-hover">Privacy policy</a>
-				<a href="/disclaimer" class="link link-hover">Disclaimer</a>
-			</nav>
-			<p>Copyright © 2023 - All rights reserved by playcardgames.io</p>
-		</footer>
-	</div>
+	<footer
+		class="footer footer-center p-10 bg-base-200 text-base-content rounded sticky top-[100vh] gap-y-8"
+	>
+		<nav class="flex flex-wrap justify-center gap-4">
+			<a href="/terms" class="link link-hover">Terms of use</a>
+			<a href="/privacy" class="link link-hover">Privacy policy</a>
+			<a href="/disclaimer" class="link link-hover">Disclaimer</a>
+		</nav>
+		<p>Copyright © 2023 - All rights reserved by playcardgames.io</p>
+	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -85,59 +85,63 @@
 	];
 </script>
 
-<Header />
-<div class="grid justify-items-center space-y-6 mt-10">
-	<h1 class="text-4xl md:text-5xl font-bold">PlayCardGames.io</h1>
-	<div>
-		<p class="px-6 text-center text-lg">
-			Enjoy an amazing collection of modern thematic solitaire card games
-		</p>
-		<p class="px-6 text-center text-lg">All games can be played with standard playing cards</p>
-	</div>
-	<nav class="flex flex-wrap justify-center gap-4 font-semibold">
-		<a href="#about" class="link link-hover">About Us</a>
-		<a href="#faq" class="link link-hover">Frequently Asked Questions</a>
-		<a href="#contact" class="link link-hover">Contact</a>
-	</nav>
-</div>
-<div class="my-20 flex flex-wrap justify-center gap-10 sm:gap-5 max-w-screen-xl mx-auto">
-	{#each games as game}
-		<a href={game.href} class="group">
-			<div
-				class="card overflow-hidden w-80 sm:w-60 bg-base-100 shadow-xl ease-in-out duration-300 group-hover:scale-105"
-			>
-				<figure>
-					<img
-						src={game.src}
-						alt={game.name}
-						class="ease-in-out duration-300 group-hover:scale-110"
-					/>
-				</figure>
-				<div class="card-body items-center text-center">
-					<h1
-						class="card-title text-3xl sm:text-xl transition duration-300 ease-in-out group-hover:text-primary"
-					>
-						{game.name}
-					</h1>
-				</div>
+<div class="bg-cards bg-fixed bg-cover">
+	<div class="bg-base-100/90 backdrop-blur-sm">
+		<Header />
+		<div class="grid justify-items-center space-y-6 py-10">
+			<h1 class="text-4xl md:text-5xl font-bold">PlayCardGames.io</h1>
+			<div>
+				<p class="px-6 text-center text-lg">
+					Enjoy an amazing collection of modern thematic solitaire card games
+				</p>
+				<p class="px-6 text-center text-lg">All games can be played with standard playing cards</p>
 			</div>
-		</a>
-	{/each}
-</div>
-<div class="grid justify-items-center space-y-6 my-10">
-	<div id="about" class="px-6 text-center">
-		<h2 class="text-4xl md:text-5xl font-bold mb-4">About Us</h2>
-		<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
-		<p>All games can be played with standard playing cards</p>
-	</div>
-	<div id="faq" class="px-6 text-center">
-		<h2 class="text-4xl md:text-5xl font-bold mb-4">Frequently Asked Questions</h2>
-		<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
-		<p>All games can be played with standard playing cards</p>
-	</div>
-	<div id="contact" class="px-6 text-center">
-		<h2 class="text-4xl md:text-5xl font-bold mb-4">Contact</h2>
-		<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
-		<p>All games can be played with standard playing cards</p>
+			<nav class="flex flex-wrap justify-center gap-4 font-semibold">
+				<a href="#about" class="link link-hover">About Us</a>
+				<a href="#faq" class="link link-hover">Frequently Asked Questions</a>
+				<a href="#contact" class="link link-hover">Contact</a>
+			</nav>
+		</div>
+		<div class="my-10 flex flex-wrap justify-center gap-10 sm:gap-5 max-w-screen-xl mx-auto">
+			{#each games as game}
+				<a href={game.href} class="group">
+					<div
+						class="card overflow-hidden w-80 sm:w-60 bg-base-100 shadow-xl ease-in-out duration-300 group-hover:scale-105"
+					>
+						<figure>
+							<img
+								src={game.src}
+								alt={game.name}
+								class="ease-in-out duration-300 group-hover:scale-110"
+							/>
+						</figure>
+						<div class="card-body items-center text-center">
+							<h1
+								class="card-title text-3xl sm:text-xl transition duration-300 ease-in-out group-hover:text-primary"
+							>
+								{game.name}
+							</h1>
+						</div>
+					</div>
+				</a>
+			{/each}
+		</div>
+		<div class="grid justify-items-center space-y-6 py-10">
+			<div id="about" class="px-6 text-center">
+				<h2 class="text-4xl md:text-5xl font-bold mb-4">About Us</h2>
+				<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
+				<p>All games can be played with standard playing cards</p>
+			</div>
+			<div id="faq" class="px-6 text-center">
+				<h2 class="text-4xl md:text-5xl font-bold mb-4">Frequently Asked Questions</h2>
+				<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
+				<p>All games can be played with standard playing cards</p>
+			</div>
+			<div id="contact" class="px-6 text-center">
+				<h2 class="text-4xl md:text-5xl font-bold mb-4">Contact</h2>
+				<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
+				<p>All games can be played with standard playing cards</p>
+			</div>
+		</div>
 	</div>
 </div>
