a69a1721373f5d5c109f8030f9ab97f5c50fafc6
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-09 18:06:41 +0100

    card capture

--- src/lib/components/GamePage.svelte
+++ src/lib/components/GamePage.svelte
@@ -1,7 +1,8 @@
 <script>
-	import Header from '$lib/components/Header.svelte';
 	import { fly } from 'svelte/transition';
 	import { bounceOut } from 'svelte/easing';
+
+	import Header from './Header.svelte';
 
 	export let title;
 </script>
@@ -12,6 +13,21 @@
 	<div class="flex flex-col h-[calc(100vh-4rem)] relative bg-emerald-600">
 		<div class="flex flex-col h-full">
 			<slot name="gameSection" />
+			<div class="flex justify-center">
+				<svg
+					fill="#FFFFFF"
+					class="w-7 h-7"
+					viewBox="0 -6 524 524"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<g id="SVGRepo_bgCarrier" stroke-width="0" />
+					<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+					<g id="SVGRepo_iconCarrier">
+						<title>down</title>
+						<path d="M64 191L98 157 262 320 426 157 460 191 262 387 64 191Z" />
+					</g>
+				</svg>
+			</div>
 		</div>
 	</div>
 	<div class="bg-base-100">
