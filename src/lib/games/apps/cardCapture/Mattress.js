907a3282730eb9fc58f33b0ad23c99c2461ee678
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-09 11:27:51 +0100

    card capture

--- src/lib/games/apps/cardCapture/Mattress.js
+++ src/lib/games/apps/cardCapture/Mattress.js
@@ -2,204 +2,110 @@ import * as PIXI from "pixi.js";
 
 export class Mattress {
     constructor(mattressContainer) {
-        this.hoveredLocation = null;
-        this.highlightColor = 0x0000FF;
 
-        // Left Monsters Zone
-        this.leftMonstersZone = new PIXI.Graphics();
-        this.leftMonstersZone.beginFill(0x000000, 0.25);
-        this.leftMonstersZone.drawRoundedRect(159, 15, 110, 233, 8);
-        this.leftMonstersZone.endFill();
-        mattressContainer.addChild(this.leftMonstersZone);
+        // Enemy Draw Pile Zone
+        this.enemyDrawPileZone = new PIXI.Graphics();
+        this.enemyDrawPileZone.beginFill(0x000000, 0.25);
+        this.enemyDrawPileZone.drawRoundedRect(0, 113, 120, 170, 8);
+        this.enemyDrawPileZone.endFill();
+        mattressContainer.addChild(this.enemyDrawPileZone);
 
-        // Center Monsters Zone
-        this.centerMonstersZone = new PIXI.Graphics();
-        this.centerMonstersZone.beginFill(0x000000, 0.25);
-        this.centerMonstersZone.drawRoundedRect(305, 15, 110, 233, 8);
-        this.centerMonstersZone.endFill();
-        mattressContainer.addChild(this.centerMonstersZone);
-
-        // Right Monsters Zone
-        this.rightMonstersZone = new PIXI.Graphics();
-        this.rightMonstersZone.beginFill(0x000000, 0.25);
-        this.rightMonstersZone.drawRoundedRect(451, 15, 110, 233, 8);
-        this.rightMonstersZone.endFill();
-        mattressContainer.addChild(this.rightMonstersZone);
-
-        // Left Attack Zone
-        this.leftAttackZone = new PIXI.Graphics();
-        this.leftAttackZone.beginFill(0x000000, 0.25);
-        this.leftAttackZone.drawRoundedRect(159, 252, 110, 194, 8);
-        this.leftAttackZone.endFill();
-        mattressContainer.addChild(this.leftAttackZone);
-
-        // Left Attack Zone Highlight
-        this.leftAttackZoneHighlight = new PIXI.Graphics();
-        this.leftAttackZoneHighlight.beginFill(this.highlightColor);
-        this.leftAttackZoneHighlight.drawRoundedRect(159, 252, 110, 194, 8);
-        this.leftAttackZoneHighlight.endFill();
-        this.leftAttackZoneHighlight.alpha = 0;
-        mattressContainer.addChild(this.leftAttackZoneHighlight);
-
-        // Center Attack Zone
-        this.centerAttackZone = new PIXI.Graphics();
-        this.centerAttackZone.beginFill(0x000000, 0.25);
-        this.centerAttackZone.drawRoundedRect(305, 252, 110, 194, 8);
-        this.centerAttackZone.endFill();
-        mattressContainer.addChild(this.centerAttackZone);
-
-        // Center Attack Zone Highlight
-        this.centerAttackZoneHighlight = new PIXI.Graphics();
-        this.centerAttackZoneHighlight.beginFill(this.highlightColor);
-        this.centerAttackZoneHighlight.drawRoundedRect(305, 252, 110, 194, 8);
-        this.centerAttackZoneHighlight.endFill();
-        this.centerAttackZoneHighlight.alpha = 0;
-        mattressContainer.addChild(this.centerAttackZoneHighlight);
-
-        // Right Attack Zone
-        this.rightAttackZone = new PIXI.Graphics();
-        this.rightAttackZone.beginFill(0x000000, 0.25);
-        this.rightAttackZone.drawRoundedRect(451, 252, 110, 194, 8);
-        this.rightAttackZone.endFill();
-        mattressContainer.addChild(this.rightAttackZone);
-
-        // Right Attack Zone Highlight
-        this.rightAttackZoneHighlight = new PIXI.Graphics();
-        this.rightAttackZoneHighlight.beginFill(this.highlightColor);
-        this.rightAttackZoneHighlight.drawRoundedRect(451, 252, 110, 194, 8);
-        this.rightAttackZoneHighlight.endFill();
-        this.rightAttackZoneHighlight.alpha = 0;
-        mattressContainer.addChild(this.rightAttackZoneHighlight);
-
-        // Monster Discard Zone
-        this.monsterDiscardZone = new PIXI.Graphics();
-        this.monsterDiscardZone.beginFill(0x000000, 0.25);
-        this.monsterDiscardZone.drawRoundedRect(597, 88, 110, 160, 8);
-        this.monsterDiscardZone.endFill();
-        mattressContainer.addChild(this.monsterDiscardZone);
-
-        // Monster Discard Zone Background
-        this.monsterDiscardZoneBg = new PIXI.Graphics();
-        this.monsterDiscardZoneBg.beginFill(0x000000, 0.25);
-        this.monsterDiscardZoneBg.drawRoundedRect(612, 103, 80, 110, 6);
-        this.monsterDiscardZoneBg.endFill();
-        mattressContainer.addChild(this.monsterDiscardZoneBg);
-
-        // Monsters Discard Label
-        this.monstersDiscardLabel = new PIXI.Text("Defeated", {
+        // Enemy Draw Pile Label
+        this.enemyDrawPileLabel = new PIXI.Text("Enemy Deck", {
             fontFamily: 'Arial',
             fontWeight: 'bold',
             fontSize: 16,
             fill: 0xFFFFFF,
             align: 'center'
         });
-        this.monstersDiscardLabel.anchor.set(0.5);
-        this.monstersDiscardLabel.x = 653;
-        this.monstersDiscardLabel.y = 230;
-        mattressContainer.addChild(this.monstersDiscardLabel);
+        this.enemyDrawPileLabel.anchor.set(0.5);
+        this.enemyDrawPileLabel.x = 60;
+        this.enemyDrawPileLabel.y = 133;
+        mattressContainer.addChild(this.enemyDrawPileLabel);
 
-        // Attack Discard Zone
-        this.attackDiscardZone = new PIXI.Graphics();
-        this.attackDiscardZone.beginFill(0x000000, 0.25);
-        this.attackDiscardZone.drawRoundedRect(597, 285, 110, 160, 8);
-        this.attackDiscardZone.endFill();
-        mattressContainer.addChild(this.attackDiscardZone);
+        // Enemy Discard Pile Zone
+        this.enemyDiscardPileZone = new PIXI.Graphics();
+        this.enemyDiscardPileZone.beginFill(0x000000, 0.25);
+        this.enemyDiscardPileZone.drawRoundedRect(600, 113, 120, 170, 8);
+        this.enemyDiscardPileZone.endFill();
+        mattressContainer.addChild(this.enemyDiscardPileZone);
 
-        // Attack Discard Zone Background
-        this.attackDiscardZoneBg = new PIXI.Graphics();
-        this.attackDiscardZoneBg.beginFill(0x000000, 0.25);
-        this.attackDiscardZoneBg.drawRoundedRect(612, 300, 80, 110, 6);
-        this.attackDiscardZoneBg.endFill();
-        mattressContainer.addChild(this.attackDiscardZoneBg);
-
-        // Attack Discard Label
-        this.attackDiscardLabel = new PIXI.Text("Discarded", {
+        // Enemy Discard Pile Label
+        this.enemyDiscardPileLabel = new PIXI.Text("Captured", {
             fontFamily: 'Arial',
             fontWeight: 'bold',
             fontSize: 16,
             fill: 0xFFFFFF,
             align: 'center'
         });
-        this.attackDiscardLabel.anchor.set(0.5);
-        this.attackDiscardLabel.x = 653;
-        this.attackDiscardLabel.y = 427;
-        mattressContainer.addChild(this.attackDiscardLabel);
+        this.enemyDiscardPileLabel.anchor.set(0.5);
+        this.enemyDiscardPileLabel.x = 660;
+        this.enemyDiscardPileLabel.y = 133;
+        mattressContainer.addChild(this.enemyDiscardPileLabel);
 
-        // Draw Pile Zone
-        this.drawPileZone = new PIXI.Graphics();
-        this.drawPileZone.beginFill(0x000000, 0.25);
-        this.drawPileZone.drawRoundedRect(12, 495, 110, 160, 8);
-        this.drawPileZone.endFill();
-        mattressContainer.addChild(this.drawPileZone);
+        // Enemy Tableau Zone
+        this.enemyTableauZone = new PIXI.Graphics();
+        this.enemyTableauZone.beginFill(0x000000, 0.25);
+        this.enemyTableauZone.drawRoundedRect(150, 113, 420, 170, 8);
+        this.enemyTableauZone.endFill();
+        mattressContainer.addChild(this.enemyTableauZone);
 
-        // Draw Pile Zone Background
-        this.drawPileZoneBg = new PIXI.Graphics();
-        this.drawPileZoneBg.beginFill(0x000000, 0.25);
-        this.drawPileZoneBg.drawRoundedRect(27, 510, 80, 110, 6);
-        this.drawPileZoneBg.endFill();
-        mattressContainer.addChild(this.drawPileZoneBg);
 
-        // Draw Pile Label
-        this.drawPileLabel = new PIXI.Text("Draw Pile", {
+
+
+
+
+
+
+
+
+
+
+        // Player Draw Pile Zone
+        this.playerDrawPileZone = new PIXI.Graphics();
+        this.playerDrawPileZone.beginFill(0x000000, 0.25);
+        this.playerDrawPileZone.drawRoundedRect(0, 360, 120, 170, 8);
+        this.playerDrawPileZone.endFill();
+        mattressContainer.addChild(this.playerDrawPileZone);
+
+        // Player Draw Pile Label
+        this.playerDrawPileLabel = new PIXI.Text("Draw Deck", {
             fontFamily: 'Arial',
             fontWeight: 'bold',
             fontSize: 16,
             fill: 0xFFFFFF,
             align: 'center'
         });
-        this.drawPileLabel.anchor.set(0.5);
-        this.drawPileLabel.x = 67;
-        this.drawPileLabel.y = 637;
-        mattressContainer.addChild(this.drawPileLabel);
+        this.playerDrawPileLabel.anchor.set(0.5);
+        this.playerDrawPileLabel.x = 60;
+        this.playerDrawPileLabel.y = 380;
+        mattressContainer.addChild(this.playerDrawPileLabel);
 
-        // Hand Zone
-        this.handZone = new PIXI.Graphics();
-        this.handZone.beginFill(0x000000, 0.25);
-        this.handZone.drawRoundedRect(210, 495, 300, 140, 8);
-        this.handZone.endFill();
-        mattressContainer.addChild(this.handZone);
+        // Player Discard Pile Zone
+        this.playerDiscardPileZone = new PIXI.Graphics();
+        this.playerDiscardPileZone.beginFill(0x000000, 0.25);
+        this.playerDiscardPileZone.drawRoundedRect(600, 360, 120, 170, 8);
+        this.playerDiscardPileZone.endFill();
+        mattressContainer.addChild(this.playerDiscardPileZone);
 
-        // Reserve Zone
-        this.reserveZone = new PIXI.Graphics();
-        this.reserveZone.beginFill(0x000000, 0.25);
-        this.reserveZone.drawRoundedRect(598, 495, 110, 160, 8);
-        this.reserveZone.endFill();
-        mattressContainer.addChild(this.reserveZone);
-
-        // Reserve Zone Highlight
-        this.reserveZoneHighlight = new PIXI.Graphics();
-        this.reserveZoneHighlight.beginFill(this.highlightColor);
-        this.reserveZoneHighlight.drawRoundedRect(598, 495, 110, 160, 8);
-        this.reserveZoneHighlight.endFill();
-        this.reserveZoneHighlight.alpha = 0;
-        mattressContainer.addChild(this.reserveZoneHighlight);
-
-        // Reserve Zone Background
-        this.reserveZoneBg = new PIXI.Graphics();
-        this.reserveZoneBg.beginFill(0x000000, 0.25);
-        this.reserveZoneBg.drawRoundedRect(613, 510, 80, 110, 6);
-        this.reserveZoneBg.endFill();
-        mattressContainer.addChild(this.reserveZoneBg);
-
-        // Reserve Label
-        this.reserveLabel = new PIXI.Text("Reserve", {
+        // Player Discard Pile Label
+        this.playerDiscardPileLabel = new PIXI.Text("Captured", {
             fontFamily: 'Arial',
             fontWeight: 'bold',
             fontSize: 16,
             fill: 0xFFFFFF,
             align: 'center'
         });
-        this.reserveLabel.anchor.set(0.5);
-        this.reserveLabel.x = 653;
-        this.reserveLabel.y = 637;
-        mattressContainer.addChild(this.reserveLabel);
-    }
+        this.playerDiscardPileLabel.anchor.set(0.5);
+        this.playerDiscardPileLabel.x = 660;
+        this.playerDiscardPileLabel.y = 380;
+        mattressContainer.addChild(this.playerDiscardPileLabel);
 
-    setHighlighted(isHighlighted) {
-        const alpha = 0.15;
-        this.leftAttackZoneHighlight.alpha = isHighlighted ? alpha : 0;
-        this.centerAttackZoneHighlight.alpha = isHighlighted ? alpha : 0;
-        this.rightAttackZoneHighlight.alpha = isHighlighted ? alpha : 0;
-        this.reserveZoneHighlight.alpha = isHighlighted ? alpha : 0;
+        // Player Tableau Zone
+        this.playerTableauZone = new PIXI.Graphics();
+        this.playerTableauZone.beginFill(0x000000, 0.25);
+        this.playerTableauZone.drawRoundedRect(150, 360, 420, 170, 8);
+        this.playerTableauZone.endFill();
+        mattressContainer.addChild(this.playerTableauZone);
     }
 }
