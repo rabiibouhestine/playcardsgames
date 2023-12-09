0407d045aca2ad5d24d9feab69647aac670f26b9
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-09 11:46:35 +0100

    card capture

--- src/lib/games/apps/cardCapture/Game.js
+++ src/lib/games/apps/cardCapture/Game.js
@@ -34,6 +34,26 @@ export class Game extends App {
 
         // add mattress
         this.mattress = new Mattress(this.mattressContainer);
+
+        // make enemy deck
+        const enemyDeck = this.dealer.shuffleCards([
+            'AS', '5S', '6S', '7S', '8S', '9S', 'TS',
+            'AC', '5C', '6C', '7C', '8C', '9C', 'TC',
+            'AH', '5H', '6H', '7H', '8H', '9H', 'TH',
+            'AD', '5D', '6D', '7D', '8D', '9D', 'TD',
+            'KD', 'KS', 'KH', 'KC',
+            'QD', 'QS', 'QH', 'QC',
+            'JD', 'JS', 'JH', 'JC'
+        ]);
+
+        // make player deck
+        const playerDeck = this.dealer.shuffleCards([
+            '2S', '3S', '4S',
+            '2C', '3C', '4C',
+            '2H', '3H', '4H',
+            '2D', '3D', '4D',
+            'J1', 'J2'
+        ]);
     }
 
 }
