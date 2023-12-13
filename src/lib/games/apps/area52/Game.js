af15840498f763d498035481cdc003c61c0ace8c
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-13 14:44:09 +0100

    area 52

--- src/lib/games/apps/area52/Game.js
+++ src/lib/games/apps/area52/Game.js
@@ -74,7 +74,20 @@ export class Game extends App {
             // onPointerDown: this.handleServe.bind(this)
         });
 
+        // selected cards
+        this.selectedCards = [];
 
+        // make shuffled aliens
+        this.aliensDeck = this.dealer.shuffleCards([
+            'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH',
+            'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD'
+        ]);
+
+        // make shuffled defenders
+        this.defendersDeck = this.dealer.shuffleCards([
+            'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS',
+            'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC'
+        ]);
 
         // enable interactions
         this.gameContainer.eventMode = 'static';
