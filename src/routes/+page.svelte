085fe5f50d64bc52a247bbde3e74ac465df497e5
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-27 16:29:01 +0100

    clean home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,26 +7,7 @@
 	import clearTheDungeonCover from '$lib/assets/gameIcons/clearTheDungeon.svg';
 	import merchantSolitaireCover from '$lib/assets/gameIcons/merchantSolitaire.svg';
 	import cardCaptureCover from '$lib/assets/gameIcons/cardCapture.svg';
-	import theSandwichGuyCover from '$lib/assets/gameIcons/theSandwichGuy.svg';
 	import area52Cover from '$lib/assets/gameIcons/area52.svg';
-
-	const classic = [
-		{
-			name: 'Klondike',
-			src: 'https://placehold.co/400x400/grey/white',
-			href: '/'
-		},
-		{
-			name: 'Freecell',
-			src: 'https://placehold.co/400x400/grey/white',
-			href: '/'
-		},
-		{
-			name: 'Spider',
-			src: 'https://placehold.co/400x400/grey/white',
-			href: '/'
-		}
-	];
 
 	const games = [
 		{
@@ -71,27 +52,6 @@
 			time: 5,
 			weight: 1.3
 		}
-		// {
-		// 	name: 'THE SANDWICH GUY',
-		// 	src: theSandwichGuyCover,
-		// 	href: '/the-sandwich-guy',
-		// 	time: 5,
-		// 	weight: 1.2
-		// },
-		// {
-		// 	name: 'LOOT THE LOOP',
-		// 	src: 'https://placehold.co/400x400/grey/white',
-		// 	href: '/',
-		// 	time: 5,
-		// 	weight: 3.0
-		// },
-		// {
-		// 	name: 'HIDE AND SEEK',
-		// 	src: 'https://placehold.co/400x400/grey/white',
-		// 	href: '/',
-		// 	time: 5,
-		// 	weight: 3.0
-		// }
 	];
 </script>
 
@@ -106,11 +66,6 @@
 				</p>
 				<p class="px-6 text-center text-lg">All games can be played with standard playing cards</p>
 			</div>
-			<nav class="flex flex-wrap justify-center gap-4 font-semibold">
-				<a href="#about" class="link link-hover">About Us</a>
-				<a href="#faq" class="link link-hover">Frequently Asked Questions</a>
-				<a href="#contact" class="link link-hover">Contact</a>
-			</nav>
 		</div>
 		<div class="my-10 flex flex-wrap justify-center gap-10 sm:gap-5 max-w-screen-xl mx-auto">
 			{#each games as game}
@@ -120,11 +75,6 @@
 		<div class="grid justify-items-center space-y-6 py-10">
 			<div id="about" class="px-6 text-center">
 				<h2 class="text-3xl md:text-4xl font-bold mb-4">About Us</h2>
-				<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
-				<p>All games can be played with standard playing cards</p>
-			</div>
-			<div id="faq" class="px-6 text-center">
-				<h2 class="text-3xl md:text-4xl font-bold mb-4">Frequently Asked Questions</h2>
 				<p>Enjoy an amazing collection of modern thematic solitaire card games</p>
 				<p>All games can be played with standard playing cards</p>
 			</div>
