e40abb77b2e27ff75e832d2bd6067486677e5aa3
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-12-29 11:01:50 +0100

    improve game header

--- src/lib/games/utils/Header.js
+++ src/lib/games/utils/Header.js
@@ -12,41 +12,53 @@ export class Header {
         this.graphic.drawRoundedRect(-356, -25, 712, 50, 8);
         this.graphic.endFill();
 
-        // Define highscore label
-        this.highscoreLabel = new PIXI.Text("- - - - - - - - -", {
+        // Define timer Text
+        this.timerText = new PIXI.Text("00:00", {
             fontFamily: 'Arial',
             fontWeight: 'bold',
             fontSize: 20,
             fill: 0xFFFFFF
         });
-        this.highscoreLabel.anchor.set(0, 0.5);
-        this.highscoreLabel.x = -340;
-        this.highscoreLabel.y = 0;
+        this.timerText.anchor.set(0.5);
+        this.timerText.x = 0;
+        this.timerText.y = 0;
 
-        // Define Restart label
-        this.restartLabel = new PIXI.Text("Restart Game", {
+        // Define highscore Text
+        this.highscoreText = new PIXI.Text("- - - - - - - - -", {
             fontFamily: 'Arial',
             fontWeight: 'bold',
             fontSize: 20,
             fill: 0xFFFFFF
         });
-        this.restartLabel.anchor.set(1, 0.5);
-        this.restartLabel.x = 340;
-        this.restartLabel.y = 0;
-        this.restartLabel.eventMode = 'static';
-        this.restartLabel.cursor = 'pointer';
-        this.restartLabel.on('pointerdown', onRestartClick);
-        this.restartLabel
-            .on('pointerover', () => {this.restartLabel.style.fill = 0x93c5fd})
-            .on('pointerout', () => {this.restartLabel.style.fill = 0xFFFFFF})
+        this.highscoreText.anchor.set(0, 0.5);
+        this.highscoreText.x = -340;
+        this.highscoreText.y = 0;
+
+        // Define Restart Text
+        this.restartText = new PIXI.Text("Restart Game", {
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            fontSize: 20,
+            fill: 0xFFFFFF
+        });
+        this.restartText.anchor.set(1, 0.5);
+        this.restartText.x = 340;
+        this.restartText.y = 0;
+        this.restartText.eventMode = 'static';
+        this.restartText.cursor = 'pointer';
+        this.restartText.on('pointerdown', onRestartClick);
+        this.restartText
+            .on('pointerover', () => {this.restartText.style.fill = 0x93c5fd})
+            .on('pointerout', () => {this.restartText.style.fill = 0xFFFFFF})
 
         // Define Container
         this.container = new PIXI.Container();
         this.container.x = 360;
         this.container.y = 45;
         this.container.addChild(this.graphic);
-        this.container.addChild(this.restartLabel);
-        this.container.addChild(this.highscoreLabel);
+        this.container.addChild(this.restartText);
+        this.container.addChild(this.highscoreText);
+        this.container.addChild(this.timerText);
 
         // add button to gameContainer
         this.gameContainer.addChild(this.container);
